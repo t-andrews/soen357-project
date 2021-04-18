6eefe3fc916db1fc77a7ea12b3b1fad1daa8f01c
Tung Leu <leudeptrai@gmail.com>
2021-04-18 02:32:22 -0400

    #8 Added course cart list

---
+++ src/components/Cart/index.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Typography from "@material-ui/core/Typography";
+
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: "#808080",
+        fontWeight: 'bold'
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+    root: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.action.hover,
+        },
+    },
+}))(TableRow);
+
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 700,
+    },
+    classTitle: {
+        fontWeight: 'bold'
+    },
+    classHeader: {
+        color: 'white'
+    }
+});
+const extractDay = (days) => {
+    let extractedDay = ""
+    days.forEach(day =>
+        extractedDay += day.substr(0,2)
+    )
+    return extractedDay
+}
+const extractInfo = (course) => {
+    let lectureTime = ""
+    let lectureRoom = ""
+    let tutorialTime = ""
+    let tutorialRoom = ""
+    let tutorialSection = ""
+    let labTime = ""
+    let labRoom = ""
+    let labSection = ""
+
+    course.sections.forEach(section => {
+        if(section.component === "Lecture") {
+            lectureTime = extractDay(section.days) + " " + section.startTime + "-" + section.endTime
+            lectureRoom = section.location
+        }
+        else if(section.component === "Tutorial") {
+            tutorialTime = extractDay(section.days) + " " + section.startTime + "-" + section.endTime
+            tutorialRoom = section.location
+            tutorialSection = section.section
+        }
+        else {
+            labTime = extractDay(section.days) + " " + section.startTime + "-" + section.endTime
+            labRoom = section.location
+            labSection = section.section
+        }
+    })
+    const info = {
+        "title": course.courseName + " " +course.courseTitle,
+        "instructor": course.instructor,
+        "lectureTime": lectureTime,
+        "lectureRoom": lectureRoom,
+        "tutorialTime": tutorialTime,
+        "tutorialRoom": tutorialRoom,
+        "tutorialSection": tutorialSection,
+        "labTime": labTime,
+        "labRoom": labRoom,
+        "labSection": labSection,
+    }
+    return info
+}
+export default function Cart(props) {
+    const classes = useStyles();
+    const course = extractInfo(props.course);
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="customized table">
+                <TableHead>
+                    <TableRow>
+                        <StyledTableCell>
+                            <Typography className={classes.classTitle} variant='h6'>{course.title}</Typography>
+                            <br/>
+                            <Typography className={classes.classHeader} variant='h7'>Intructor: {course.instructor}</Typography>
+                        </StyledTableCell>
+                        <StyledTableCell align="right">
+                            <Typography className={classes.classHeader} variant='h7'>
+                                {course.lectureTime}
+                            <br/>
+                                {course.lectureRoom}
+                            </Typography>
+                        </StyledTableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                <StyledTableRow>
+                    <StyledTableCell component="th" scope="row">
+                        {course.tutorialSection}(Tutorial)
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                        {course.tutorialTime}
+                        <br/>
+                        {course.tutorialRoom}
+                    </StyledTableCell>
+                </StyledTableRow>
+                <StyledTableRow>
+                    <StyledTableCell component="th" scope="row">
+                        {course.labSection}(Laboratory)
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                        {course.labTime}
+                        <br/>
+                        {course.labRoom}
+                    </StyledTableCell>
+                </StyledTableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
--- src/views/CourseCart/index.js
+++ src/views/CourseCart/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
-import {makeStyles} from "@material-ui/core/styles";
-import {Paper} from "@material-ui/core";
-
+import {
+    Button,
+    makeStyles,
+    Paper,
+    Table,
+    TableBody, TableCell,
+    TableContainer,
+    TableHead,
+    TableRow, Tooltip,
+    withStyles
+} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Cart from "../../components/Cart";
+import Radio from '@material-ui/core/Radio';
+import DeleteIcon from '@material-ui/icons/Delete';
 const useStyles = makeStyles({
     root: {
         flexGrow: 1,
@@ -15,16 +28,77 @@ const useStyles = makeStyles({
         width: '100%',
         height: '15px',
     },
+    title: {
+        color: '#912338',
+        fontWeight: 'bold',
+        paddingTop: '10px',
+        paddingBottom: '20px',
+        paddingLeft: '20px',
+        float: 'left',
+
+    },
+    classHeader: {
+        fontWeight: 'bold',
+        float: 'left'
+    },
+    left: {
+      float: 'left'
+    },
+    right: {
+        float: 'left'
+    },
+    table: {
+        maxWidth: 800,
+    },
+    card:{
+        width: '50%',
+    },
+    form:{
+        width: '50%',
+
+    }
 });
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+        backgroundColor: '#808080' ,
+
+    },
+    body: {
+        fontSize: 14,
+    },
+}))(TableCell);
 
 export default function CourseCart() {
     const classes = useStyles();
+    const courses = require("../../services/courseCart.json")
 
     return(
         <div>
             <Paper className={classes.paper} elevation={0}/>
+            <Typography className={classes.title} variant='h5'>Course cart</Typography>
             <div className={classes.root}>
-                <h1>Code Here</h1>
+                <div className={classes.card}>
+                    <Table>
+                        {Object.keys(courses).map(key => (
+                            <TableRow>
+                                <TableCell>
+                                    <Radio/>
+                                </TableCell>
+                                <TableCell>
+                                    <Cart course={courses[key]}/>
+                                </TableCell>
+                                <TableCell>
+                                    <Button>
+                                        <DeleteIcon/>
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))
+
+                        }
+                    </Table>
+
+                </div>
             </div>
         </div>
 
