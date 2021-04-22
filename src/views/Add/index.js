862307dfba3b2e3316bd28b611c769ab016432b1
itsShaVawn <siobhan.cousineau@gmail.com>
2021-04-21 23:26:55 -0400

    7_Add_Combined

--- src/views/Add/index.js
+++ src/views/Add/index.js
@@ -3,22 +3,32 @@ import {makeStyles} from "@material-ui/core/styles";
 import {
     Button,
     Checkbox,
-    Dialog, DialogContent, DialogTitle,
+    Dialog, DialogContent, DialogTitle, FormControl,
     FormControlLabel,
     FormGroup,
     Grid,
-    InputBase, List, ListItem,
     Paper,
     TextField, Tooltip,
     withStyles
 } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import {Cancel, CheckCircle, PauseCircleFilled, Search} from "@material-ui/icons";
-import SearchResultsShell from "../../components/SearchResultsShell";
 import ModalContent from "../../components/AddModalContent";
 import * as AllCourses from "../../services/allCourses.json";
 import SelectSearch, {fuzzySearch} from "react-select-search";
 import '../../Search.css';
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import Box from "@material-ui/core/Box";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import PropTypes from "prop-types";
+import TableHead from "@material-ui/core/TableHead";
 
 const useStyles = makeStyles({
     root: {
@@ -32,6 +42,9 @@ const useStyles = makeStyles({
         backgroundColor: 'white',
         width: '100%',
         height: '15px',
+    },
+    paperList: {
+        border: '1px solid black'
     },
     title: {
         color: '#912338',
@@ -86,6 +99,37 @@ const useStyles = makeStyles({
     }
 });
 
+const useAccordionStyles = makeStyles({
+    root: {
+        width: '100%',
+    },
+    accordion: {
+        backgroundColor: '#6e6e6e'
+    },
+    headerFont: {
+        color: 'white',
+        fontWeight: 'bold',
+    }
+});
+
+const useRowStyles = makeStyles({
+    root: {
+        width: '100%'
+    },
+    courseCard: {
+        width: '100%'
+    },
+    course_name:{
+        fontWeight: "bold",
+        fontSize: "20px"
+    },
+    status: {
+        stroke: 'white',
+        strokeWidth: 2,
+        marginLeft: '10px'
+    }
+});
+
 const CustomTextField = withStyles({
     root: {
         '& .MuiOutlinedInput-root': {
@@ -111,21 +155,168 @@ const BurgundyCheckbox = withStyles({
     checked: {},
 })((props) => <Checkbox color="default" {...props} />);
 
+function createData(value, name, location, time, instructor, color, status) {
+    return {
+        value,
+        name,
+        location,
+        time,
+        color,
+        instructor,
+        status,
+        subSection: [
+            {
+                name: "Y YX (Laboratory)",
+                time: "We 4:15PM - 6:15PM ",
+                room: "H 967 SGW",
+                status: 'wait'
+            },
+            {
+                name: "Y YY (Laboratory)",
+                time: "Th 4:15PM - 6:15PM ",
+                room: "H 967 SGW",
+                status: 'closed'
+            },
+            {
+                name: "Y YX (Laboratory)",
+                time: "Fri 4:15PM - 6:15PM ",
+                room: "H 967 SGW",
+                status: 'open'
+            },
+            {
+                name: "Y YJ (Tutorial)",
+                time: "Mo 4:15PM - 6:15PM ",
+                room: 'MB 111 SGW',
+                status: 'open'
+            },
+            {
+                name: "Y YK (Tutorial)",
+                time: "Tu 4:15PM - 6:15PM ",
+                room: 'MB 111 SGW',
+                status: 'closed'
+            },
+            {
+                name: "Y YL (Tutorial)",
+                time: "We 4:15PM - 6:15PM ",
+                room: 'MB 111 SGW',
+                status: 'open'
+            },
+            {
+                name: "Y YM (Tutorial)",
+                time: "Th 4:15PM - 6:15PM ",
+                room: 'MB 111 SGW',
+                status: 'wait'
+            }
+        ],
+    };
+}
+
+function Row(props) {
+    const { row } = props;
+    const classes = useRowStyles();
+
+    return (
+        <FormControl className={classes.courseCard}>
+            <TableContainer className={classes.root}>
+                <TableRow style={{backgroundColor: row.color}}>
+                    <TableCell>
+                        <FormControlLabel control={<BurgundyCheckbox />} value={row.value}/>
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                        <span className={classes.course_name}>{row.name}</span>
+                        {
+                            row.status === 'open' ? <Tooltip title={'Open'}><CheckCircle className={classes.status} style={{ color: "green" }}/></Tooltip>
+                                : (
+                                    row.status === 'wait' ? <Tooltip title={'Waitlisted'}><PauseCircleFilled className={classes.status} style={{ color: "#FFB300" }}/></Tooltip>
+                                        : (
+                                            row.status  === 'closed' ? <Tooltip title={'Closed'}><Cancel className={classes.status} style={{ color: "red" }}/></Tooltip>
+                                                : (null)
+                                        )
+                                )
+                        }
+                        <br/>
+                        <span style={{color:"white"}}>Instructor: {row.instructor}</span>
+                    </TableCell>
+                    <TableCell align="right" style={{color:"white"}}>
+                        {row.time}
+                        <br/>
+                        Room: {row.location}
+                    </TableCell>
+                </TableRow>
+                <TableRow style={{borderColor:row.color}}>
+                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                        <Box margin={1}>
+                            <Table size="small" aria-label="purchases">
+                                <TableBody>
+                                    {row.subSection.map((subsection) => (
+                                        <TableRow key={subsection.name}>
+                                            <TableCell component="th" scope="row">
+                                                <BurgundyCheckbox />
+                                                {subsection.name}
+                                                {
+                                                    subsection.status === 'open' ? <Tooltip title={'Open'}><CheckCircle style={{ color: "green", marginLeft: '10px', marginRight: '261px'}}/></Tooltip>
+                                                        : (
+                                                            subsection.status === 'wait' ? <Tooltip title={'Waitlisted'}><PauseCircleFilled style={{ color: "#FFB300", marginLeft: '10px' }}/></Tooltip>
+                                                                : (
+                                                                    subsection.status  === 'closed' ? <Tooltip title={'Closed'}><Cancel style={{ color: "red", marginLeft: '10px' }}/></Tooltip>
+                                                                        : (null)
+                                                                )
+                                                        )
+                                                }
+                                            </TableCell>
+                                            <TableCell align="right">
+                                                {subsection.time}
+                                                <br/>
+                                                Room: {subsection.room}
+                                            </TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </TableCell>
+                </TableRow>
+            </TableContainer>
+        </FormControl>
+    );
+}
+
+Row.propTypes = {
+    row: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        location: PropTypes.string.isRequired,
+        time: PropTypes.string.isRequired,
+        instructor: PropTypes.string.isRequired,
+        subSection: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.number.isRequired,
+                time: PropTypes.string.isRequired,
+                room: PropTypes.string.isRequired
+            })
+        ).isRequired,
+    }).isRequired
+};
+
+const rows = [
+    createData("1", "SOEN 357 Y - UI Design", 'H 629 SGW', 'TuTh 14:15PM - 15:30PM','Some Staff', '#6e6e6e', 'open'),
+    createData("2", "SOEN 357 X - UI Design",'H 835 SGW', 'MoWe 10:15AM - 11:30AM','Some Staff','#6e6e6e', 'closed'),
+];
+
 export default function Add() {
     const classes = useStyles();
+    const accordionClasses = useAccordionStyles();
 
+    const courses = AllCourses.courses.map(c => ({name: c.courseName, value: c.courseName}));
+    const [selectedCourse, setSelectedCourse] = React.useState({});
+    const [openModal, setOpenModal] = React.useState(false);
     const [state, setState] = React.useState({
         L200: false, L300: false, L400: false, L500: false, L600: false, L700: false, L800: false,
         Sunday: false, Monday: false, Tuesday: false, Wednesday: false, Thursday: false, Friday: false, Saturday: false,
     });
 
-    const [selectedCourse, setSelectedCourse] = React.useState({});
-
     const handleChange = (event) => {
         setState({ ...state, [event.target.name]: event.target.checked });
     };
-
-    const [openModal, setOpenModal] = React.useState(false);
 
     const handleOpenModal = () => {
         setOpenModal(true);
@@ -135,7 +326,9 @@ export default function Add() {
         setOpenModal(false);
     };
 
-    const courses = AllCourses.courses.map(c => ({name: c.courseName, value: c.courseName}));
+    const handleSearch = (event) => {
+
+    }
 
     return(
         <div>
@@ -298,11 +491,13 @@ export default function Add() {
                                     </Tooltip>
                                     <Grid item style={{width: '100%'}}>
                                         <Grid container direction='row' justify='space-between'>
+                                            <Tooltip title={'For Visual Purposes ONLY'} className={classes.tooltip}>
+                                                <Grid item>
+                                                    <Button className={classes.button} variant='contained'>Clear</Button>
+                                                </Grid>
+                                            </Tooltip>
                                             <Grid item>
-                                                <Button className={classes.button} variant='contained'>Clear</Button>
-                                            </Grid>
-                                            <Grid item>
-                                                <Button className={classes.button} variant='contained'>Search</Button>
+                                                <Button className={classes.button} variant='contained' onClick={handleSearch}>Search</Button>
                                             </Grid>
                                         </Grid>
                                     </Grid>
@@ -327,7 +522,68 @@ export default function Add() {
                         </Grid>
                         <Grid container direction='column' spacing={3} alignItems='flex-end'>
                             <Grid item>
-                                <SearchResultsShell />
+                                <div className={accordionClasses.root}>
+                                    <Accordion className={accordionClasses.accordion}>
+                                        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-label="Expand"
+                                                          aria-controls="additional-actions1-content" id="additional-actions1-header"
+                                        >
+                                            <Grid container direction='row' justify='space-between' className={accordionClasses.headerFont}>
+                                                <Grid item>
+                                                    <Typography variant='h5'>SOEN 357 - UI Design</Typography>
+                                                </Grid>
+                                                <Grid item style={{marginLeft: '1%'}}>
+                                                    <Typography variant='h5'>3.00 Units</Typography>
+                                                </Grid>
+                                            </Grid>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <div>
+                                                <Paper className={classes.paperList} variant='outlined'>
+                                                    <TableContainer>
+                                                        <Table>
+                                                            <TableHead/>
+                                                            <TableBody>
+                                                                {rows.map((row) => (
+                                                                    <Row key={row.name} row={row}/>
+                                                                ))}
+                                                            </TableBody>
+                                                        </Table>
+                                                    </TableContainer>
+                                                </Paper>
+                                            </div>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                    <Accordion className={accordionClasses.accordion}>
+                                        <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-label="Expand"
+                                                          aria-controls="additional-actions2-content" id="additional-actions2-header"
+                                        >
+                                            <Grid container direction='row' justify='space-between' className={accordionClasses.headerFont}>
+                                                <Grid item>
+                                                    <Typography variant='h5'>SOEN 363 - Databases</Typography>
+                                                </Grid>
+                                                <Grid item style={{marginLeft: '1%'}}>
+                                                    <Typography variant='h5'>3.00 Units</Typography>
+                                                </Grid>
+                                            </Grid>
+                                        </AccordionSummary>
+                                        <AccordionDetails>
+                                            <div>
+                                                <Paper className={classes.paperList} variant='outlined'>
+                                                    <TableContainer>
+                                                        <Table>
+                                                            <TableHead/>
+                                                            <TableBody>
+                                                                {rows.map((row) => (
+                                                                    <Row key={row.name} row={row}/>
+                                                                ))}
+                                                            </TableBody>
+                                                        </Table>
+                                                    </TableContainer>
+                                                </Paper>
+                                            </div>
+                                        </AccordionDetails>
+                                    </Accordion>
+                                </div>
                             </Grid>
                             <Grid item>
                                 <Button className={classes.button} variant='contained' onClick={handleOpenModal}>Add</Button>
